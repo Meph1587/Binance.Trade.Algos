67e253707ef091cd17b4c28aa6a22074afa161cf
filippo888 <36636060+filippo888@users.noreply.github.com>
2018-08-31 17:20:26 +0200

    Update finalDoublingMethod.py

--- algo/finalDoublingMethod.py
+++ algo/finalDoublingMethod.py
@@ -14,46 +14,57 @@ closePrice= coin_tb['Close'].astype(float)
 
 def doublingMethod(wall, quant):
 
- 	wallet= wall
- 	quantity= quant
- 	x= closePrice[0]
- 	sellPhase= False
- 	quantityBuy= quantity/2
- 	investment= 0
+  wallet= wall
+  quantityBuy= quant
+  x= closePrice[0] #first price
+  sellPhase= False
+  investment= 0
 
-	print "Wallet:", wallet, "Time:", time[0]
-	print " "
+  print "Wallet:", wallet, "Time:", time[0]
+  print " "
 
-	for i in range(len(closePrice)):
+  for i in range(len(closePrice)):
 
-		if closePrice[i]< 0.99* x:
-  			quantityBuy*=2
-  			investment= (investment* 0.99)+ quantityBuy
-  			wallet-= quantityBuy 
-  			print "BUY, ", "Price:", x, "Time:", time[0], "Quantity:", quantityBuy
-	   		print "Wallet:", wallet
- 			x= closePrice[i]
-       		sellPhase= True
+    if closePrice[i]< 0.99* x:   
+      investment= (investment* 0.99)+ quantityBuy #calculate the value of the money invested
+      wallet-= quantityBuy #subtract money invested
+      print "BUY, ", "Price:",closePrice[i] , "Time:", time[i], "Quantity:", quantityBuy
+      print "Wallet:", wallet
+      quantityBuy*= 2 #doublig money invest
+      x= closePrice[i]
+      sellPhase= True
 
-		if closePrice[i]> 1.01*x and sellPhase== True:
-    		wallet+= investment* 1.01
-    		print "SELL, ", "Price:", closePrice[i], "Time:", time[i], "Quantity:", investment* 1.01
-    		print "Wallet:", wallet
-    		print " "
-    		x= closePrice[i]
-    		sellPhase= False
-       		quantityBuy= quantity/2
-       		investment= 0
+    if closePrice[i]> 1.01*x and sellPhase== True:
 
-      	++i
+      wallet+= investment* 1.01 #earn money invested
+      print " "
+      print "SELL, ", "Price:", closePrice[i], "Time:", time[i], "Quantity:", investment* 1.01
+      print "Wallet:", wallet
+      print " "
+      x= closePrice[i]
+      sellPhase= False
+      quantityBuy= quantity/2
+      investment= 0
 
-    print " "  
-    lenEndTime= len(closePrice)-1
-    print "Wallet:", wallet, "Start time:", time[0], "End time:", time[lenEndTime]
+    ++i
+
+  print " "  
+  lenEndTime= len(closePrice)-1
+  print "Wallet:", wallet, "Start time:", time[0], "End time:", time[lenEndTime]
+
+  return
 
 
 #example
-doublingMethod(10000, 500)  
+doublingMethod(10000, 100)  
+
+
+
+
+
+
+
+
 
 
 
