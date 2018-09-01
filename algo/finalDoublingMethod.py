82921c2f154cb04c80249babc7e950c9fc6fbd9d
filippo888 <36636060+filippo888@users.noreply.github.com>
2018-09-01 12:08:05 +0200

    Update finalDoublingMethod.py

--- algo/finalDoublingMethod.py
+++ algo/finalDoublingMethod.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 client= Client("", "")
 
-coin= client.get_historical_klines(symbol='ETHUSDT', interval= '5m', start_str= '2018.07.01', end_str= '2018.08.01')
+coin= client.get_historical_klines(symbol='ETHUSDT', interval= '5m', start_str= '2018.08.01', end_str= '2018.08.02')
 coin_tb= pd.DataFrame(coin, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
 
 time= pd.to_datetime(coin_tb['Open time'], unit='ms')
@@ -18,45 +18,106 @@ def doublingMethod(wall, quant):
   quantityBuy= quant
   x= closePrice[0] #first price
   sellPhase= False
-  investment= 0
+  crypto= 0
+
+  #for the plot 
+  buyTime= []
+  buyPrice= []
+
+  sellTime= [] 
+  sellPrice= []
+
 
   print "Wallet:", wallet, "Time:", time[0]
   print " "
 
   for i in range(len(closePrice)):
 
-    if closePrice[i]< 0.99* x:   
-      investment= (investment* 0.99)+ quantityBuy #calculate the value of the money invested
+    if closePrice[i]< 0.99* x or sellPhase== False:   
+      crypto+= quantityBuy/closePrice[i] #calculate the value of the money invested
       wallet-= quantityBuy #subtract money invested
       print "BUY, ", "Price:",closePrice[i] , "Time:", time[i], "Quantity:", quantityBuy
       print "Wallet:", wallet
+      print "Crypto", crypto
       quantityBuy*= 2 #doublig money invest
       x= closePrice[i]
       sellPhase= True
 
+      buyTime.append(time[i])
+      buyPrice.append(closePrice[i])
+
+
+
     if closePrice[i]> 1.01*x and sellPhase== True:
 
-      wallet+= investment* 1.01 #earn money invested
+      wallet+= crypto*closePrice[i] #earn money invested
       print " "
-      print "SELL, ", "Price:", closePrice[i], "Time:", time[i], "Quantity:", investment* 1.01
+      print "SELL, ", "Price:", closePrice[i], "Time:", time[i], "Quantity:", crypto*closePrice[i]
       print "Wallet:", wallet
       print " "
       x= closePrice[i]
       sellPhase= False
       quantityBuy= quant
-      investment= 0
+      crypto= 0
+
+      sellTime.append(time[i])
+      sellPrice.append(closePrice[i])
 
     ++i
 
   print " "  
   lenEndTime= len(closePrice)-1
-  print "Wallet:", wallet, "Start time:", time[0], "End time:", time[lenEndTime]
+  print "Wallet:", wallet , "Start time:", time[0], "End time:", time[lenEndTime]
+
+  plt.plot(time,closePrice,'g',label= 'Price')
+  plt.plot(buyTime,buyPrice,'d',label= 'Buy')
+  plt.plot(sellTime, sellPrice,'d',label= 'Sell')
+  plt.ylabel('PRICE')
+  plt.xlabel('TIME')
+  plt.legend()
+  plt.grid(True)
+  plt.show()
 
   return
 
 
 #example
 doublingMethod(10000, 100)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
